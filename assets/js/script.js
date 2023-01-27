86df0e5f0c81c60512766c0c4ac368b4c78f7ca9
FlorSing <fpsingcak@gmail.com>
2023-01-27 23:36:15 +0000

    made the text area and button appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+//top header date and time
+
 $('#currentDay').text("Today's date is:");
 
+//display the actual running time on top
 var datetime = null,
         date = null;
 
@@ -14,35 +17,63 @@ $(document).ready(function(){
     setInterval(update, 1000);
 });
 
-//determine time blocks for past present future
-
-
+//determine time blocks for past present future and setup scheduler
 
 var containerList = $(".container");
 var timeCurrent = moment().format('hh a')
 var timeSlot = moment().format('hh a');
-listEntry09 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry10 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry11 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry12 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry01 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry02 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry03 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry04 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
-listEntry05 = $("<li>").addClass('row time-block hour').text(timeSlot).appendTo(containerList);
+var timeSlot09 = moment().hour('09').format('hh a');
+var timeSlot10 = moment().hour('10').format('hh a');
+var timeSlot11 = moment().hour('11').format('hh a');
+var timeSlot12 = moment().hour('12').format('hh a');
+var timeSlot01 = moment().hour('13').format('hh a');
+var timeSlot02 = moment().hour('14').format('hh a');
+var timeSlot03 = moment().hour('15').format('hh a');
+var timeSlot04 = moment().hour('16').format('hh a');
+var timeSlot05 = moment().hour('17').format('hh a');
+
+listEntry09 = $("<li>").addClass('row time-block hour').text(timeSlot09).appendTo(containerList);
+listEntry10 = $("<li>").addClass('row time-block hour').text(timeSlot10).appendTo(containerList);
+listEntry11 = $("<li>").addClass('row time-block hour').text(timeSlot11).appendTo(containerList);
+listEntry12 = $("<li>").addClass('row time-block hour').text(timeSlot12).appendTo(containerList);
+listEntry01 = $("<li>").addClass('row time-block hour').text(timeSlot01).appendTo(containerList);
+listEntry02 = $("<li>").addClass('row time-block hour').text(timeSlot02).appendTo(containerList);
+listEntry03 = $("<li>").addClass('row time-block hour').text(timeSlot03).appendTo(containerList);
+listEntry04 = $("<li>").addClass('row time-block hour').text(timeSlot04).appendTo(containerList);
+listEntry05 = $("<li>").addClass('row time-block hour').text(timeSlot05).appendTo(containerList);
+
+if (timeCurrent > timeSlot09){
+    listEntry09.addClass('.past');
+    console.log('white');}
+else if (timeCurrent === timeSlot09){
+    listEntry09.addClass('.future');
+    console.log('green');}
+ else {listEntry09.addClass('.present');
+    console.log('red');}
+;
+var textArea = $('<textarea>');
+textArea.addClass('textarea').appendTo(listEntry09);
+
+var saveTask = $('<button>');
+saveTask.addClass('saveBtn').appendTo(listEntry09);
+
+ 
+// textArea.text(enterTask)
 
 
+// <script>
+//     var allInputs = $( ":input" );
+//     var formChildren = $( "form > *" );
+//     $( "#messages" ).text( "Found " + allInputs.length + " inputs and the form has " +
+//       formChildren.length + " children." );
+     
+//     $( "form" ).submit(function( event ) {
+//       event.preventDefault();
+//     });
+//     </script>
+    
 
-
-
-listEntry1.addClass('row time-block hour');
-listEntry2.addClass('row time-block hour');
-$(listEntry1).text('testing');
-$(listEntry2).text('testing2');
-listEntry1.appendTo(containerList);
-listEntry2.appendTo(containerList);
-
-
+    
 // var formEl = $('#skills-form');
 // var nameInputEl = $('#skill-name');
 // var dateInputEl = $('#datepicker');
